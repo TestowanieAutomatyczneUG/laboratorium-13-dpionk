45b735bd08b0b75d27cd378ec293ba6131b2b62a
Daria Pionk <dariapionk13@gmail.com>
2022-01-09 23:50:07 +0100

    Zadanie 1

---
+++ features/steps/CheckPassword.py
@@ -0,0 +1,34 @@
+from behave import *
+from func.CheckPassword import CheckPassword
+from assertpy import assert_that
+
+@given("there is a password checker")
+def step_imp(context):
+	context.checkPassword = CheckPassword()
+
+@when("the password is just one lower letter")
+def step_imp(context):
+	context.password = "k"
+
+@when("the password is less than 8 characters")
+def step_imp(context):
+	context.password = "kF$4"
+
+@when("the password is more than 8 characters, at least one lower letter and number")
+def step_imp(context):
+	context.password = "gdfgfd345435"
+
+@when("the password is more than 8 characters, at least one lower, upper letter and number")
+def step_imp(context):
+	context.password = "sdfdFFFF5464565"
+
+@when("the password is 8 characters, with lower, at least one lower, upper letter, special character and number")
+def step_imp(context):
+	context.password = "dsfsdFFFF$$$$2021"
+
+@then("the password is invalid")
+def step_imp(context):
+	assert context.checkPassword.ValidPassword(context.password) == False
+@then("the password is valid")
+def test_imp(context):
+	assert context.checkPassword.ValidPassword(context.password) == True
